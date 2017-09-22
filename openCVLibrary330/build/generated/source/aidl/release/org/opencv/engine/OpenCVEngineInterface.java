93b62e8d1085670ce29542c5cea07ccc887003d2
dzoli <bje.no9@gmail.com>
2017-09-22 14:19:31 +0200

    added provided dependency to documentdetector from parent (app) module.

--- openCVLibrary330/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary330/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Laszlo\\Desktop\\praksaB\\DocumentRecognitionSample\\openCVLibrary330\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Novica\\Desktop\\Execom-praksa\\DocumentRecognitionLib\\openCVLibrary330\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
